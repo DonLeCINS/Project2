63665c9373682d2b72c670143550785a0949717c
Don Le <Dle11@mail.csuchico.edu>
2017-03-31 12:18:14 -0700

    project2

---
+++ functions.js
@@ -0,0 +1,78 @@
+function answer()
+{
+  var answerEntered = document.getElementById("myNumber").value;
+  if (answerEntered == 8)
+  {
+  document.getElementById("numberError").innerHTML="Correct answer";
+  }
+  else if (isNaN(answerEntered))
+  {
+  alert("Enter price as number");
+  }
+  else
+  {
+  document.getElementById("numberError").innerHTML="Incorrect answer";
+  }
+}
+function answer2()
+{
+  var answerEntered = document.getElementById("myNumber").value
+  if (answerEntered == 40)
+  {
+    document.getElementById("newError").innerHTML="Correct answer";
+  }
+  else if (isNaN(answerEntered))
+  {
+    alert("Enter price as number");
+  }
+  else
+  {
+    document.getElementById("newError").innerHTML="Incorrect answer";
+  }
+}
+function answer3()
+{
+var answerEntered = document.getElementById("myNumber").value
+if (answerEntered == 50)
+{
+  document.getElementById("numberError").innerHTML="Correct answer";
+}
+else if (isNaN(answerEntered))
+{
+  alert("Enter price as number");
+}
+else
+{
+  document.getElementById("numberError").innerHTML="Incorrect answer";
+}
+}
+function yourScore()
+{
+  var endScore;
+  var theScore = getCookie("score");
+  theScore = Number(theScore);
+  endScore = theScore;
+  document.getElementById("tot").innerHTML = (endScore);
+
+}
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+}
